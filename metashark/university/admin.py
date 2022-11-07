161e82c58582b282e3cf28843cb10c1ddc35c5df
Roman Iksanov <Iksanov1998@inbox.ru>
2022-11-08 00:33:54 +0500

    Созданы 3 модели

--- metashark/university/admin.py
+++ metashark/university/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+class Direction_of_studyingAdmin(admin.ModelAdmin):
+    list_display = ("direction_of_studying",)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("subject_name", "direction")
+
+
+class CuratorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "patronymic", "direction_of_studying")
+
+
+admin.site.register(Direction_of_studying, Direction_of_studyingAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Curator, CuratorAdmin)
--- metashark/university/models.py
+++ metashark/university/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
+
 # Create your models here.
+class Direction_of_studying(models.Model):
+    direction_of_studying = models.CharField(max_length=255, blank=False, unique=True, verbose_name="Направление подготовки")
+
+    def __str__(self):
+        return self.direction_of_studying
+
+    class Meta:
+        verbose_name = "Направление подготовки"
+        verbose_name_plural = "Направления подготовки"
+
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=255, blank=False, unique=True, verbose_name="Предмет")
+    direction = models.ForeignKey(Direction_of_studying,
+                                  on_delete=models.CASCADE,
+                                  blank=False,
+                                  verbose_name="Направление")
+
+    def __str__(self):
+        return self.subject_name
+
+    class Meta:
+        verbose_name = "Предмет"
+        verbose_name_plural = "Предметы"
+
+
+class Curator(models.Model):
+    first_name = models.CharField(max_length=50, blank=False, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, blank=False, verbose_name="Фамилия")
+    patronymic = models.CharField(max_length=50, blank=True, verbose_name="Отчество")
+    direction_of_studying = models.OneToOneField(Direction_of_studying,
+                                                 on_delete=models.SET_NULL,
+                                                 blank=True,
+                                                 null=True,
+                                                 verbose_name="Направление")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        verbose_name = "Куратор"
+        verbose_name_plural = "Кураторы"
+
+
+# class Student(models.Model):
+#     first_name = models.CharField(max_length=50, blank=False, verbose_name="Имя")
+#     last_name = models.CharField(max_length=50, blank=False, verbose_name="Фамилия")
+#     patronymic = models.CharField(max_length=50, blank=True, verbose_name="Отчество")
+#     studying_group = models.OneToOneField("Group", on_delete)
