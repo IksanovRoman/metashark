c9a585c5516d8087f52f6a64f1b5dbcd1891d5ca
Roman Iksanov <Iksanov1998@inbox.ru>
2022-11-08 01:31:05 +0500

    Созданы 5 моделей

--- metashark/university/admin.py
+++ metashark/university/admin.py
@@ -10,12 +10,24 @@ class Direction_of_studyingAdmin(admin.ModelAdmin):
 
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ("subject_name", "direction")
+    ordering = ["direction", ]
 
 
 class CuratorAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "patronymic", "direction_of_studying")
+    list_display = ("last_name", "first_name", "patronymic", "direction_of_studying")
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "patronymic", "studying_group")
+
+
+class Education_groupAdmin(admin.ModelAdmin):
+    list_display = ("group_number", "direction")
+    ordering = ["group_number", ]
 
 
 admin.site.register(Direction_of_studying, Direction_of_studyingAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Curator, CuratorAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Education_group, Education_groupAdmin)
