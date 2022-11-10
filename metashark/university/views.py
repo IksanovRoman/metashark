b9c237b56557bfdfdcef03e07fb24b8868a2dddf
Roman Iksanov <Iksanov1998@inbox.ru>
2022-11-10 18:39:10 +0500

    Сделал формирование excel файла

--- metashark/university/views.py
+++ metashark/university/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from openpyxl import Workbook
+from openpyxl.styles import Alignment, Font
+from openpyxl.utils import get_column_letter
 from rest_framework.generics import *
 from rest_framework.permissions import *
 from rest_framework.response import Response
@@ -18,9 +20,8 @@ from university.serializers import *
 
 
 def excel(request):
-
     direction_queryset = Direction_of_studying.objects.all()
-    students_queryset = Student.objects.all()
+    groups_queryset = Education_group.objects.all()
 
     response = HttpResponse(
         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -30,39 +31,35 @@ def excel(request):
     )
     workbook = Workbook()
 
-    worksheet = workbook.create_sheet(title="Направления", index=0)
-    worksheet2 = workbook.create_sheet(title="Студенты", index=1)
+    worksheet = workbook.create_sheet(title="Учебные направления", index=0)
+    worksheet2 = workbook.create_sheet(title="Учебные группы", index=1)
+    wrapped_alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
 
-
-    # Define the titles for worksheet
-    columns = [
+    # Define the titles for worksheet and worksheet2
+    worksheet_title = [
         'ID',
         'Куратор',
         'Направление',
         'Дисциплины',
     ]
 
-
-    columns2 = [
+    worksheet2_title = [
         'ID',
-        'Имя',
-        'Фамилия',
-        'Отчество',
-        'Группа',
+        'Номер группы',
+        'Количество свободных мест',
+        'Список студентов'
     ]
 
-    row_num2 = 1
-
     # Assign the titles for each cell of the header
-    for col_num, column_title in enumerate(columns, 1):
-        cell = worksheet.cell(row=1, column=col_num)
-        cell.value = column_title
+    def set_worksheet_title(worksheet, title):
+        for col_num, column_title in enumerate(title, 1):
+            cell = worksheet.cell(row=1, column=col_num)
+            cell.value = column_title
+            cell.alignment = wrapped_alignment
+            cell.font = Font(bold=True)
 
-
-    # Assign the titles for each cell of the header
-    for col_num, column_title in enumerate(columns2, 1):
-        cell = worksheet2.cell(row=1, column=col_num)
-        cell.value = column_title
+    set_worksheet_title(worksheet, worksheet_title)
+    set_worksheet_title(worksheet2, worksheet2_title)
 
     row_num = 2
     # Iterate through all directions
@@ -74,45 +71,68 @@ def excel(request):
         except ObjectDoesNotExist:
             curator = "Нет куратора"
 
-        row = [
+        data = [
             direction.pk,
             curator,
             direction.direction,
         ]
-        row2 = direction.get_dir.all()
+        data2 = direction.get_dir.all()
 
         # Assign the data for each cell of the row
-        for col_num, cell_value in enumerate(row, 1):
+        for col_num, cell_value in enumerate(data, 1):
             cell = worksheet.cell(row=row_num, column=col_num)
             cell.value = cell_value
+            cell.alignment = wrapped_alignment
 
-        for col_num, cell_value in enumerate(row2, 1):
-            cell = worksheet.cell(row=row_num, column=len(row)+1)
+        for col_num, cell_value in enumerate(data2, 1):
+            cell = worksheet.cell(row=row_num, column=len(data) + 1)
             cell.value = cell_value.subject_name
+            cell.alignment = wrapped_alignment
             row_num += 1
 
-    # Iterate through all directions
-    for student in students_queryset:
-        row_num += 1
+        if not data2:
+            row_num += 1
 
+    row_num = 2
+    # Iterate through all directions
+    for groups in groups_queryset:
+
+        nums_of_students_in_group = groups.student_set.all().count()
+        left_students = 20 - nums_of_students_in_group
         # Define the data for each cell in the row
-        row = [
-            student.pk,
-            student.first_name,
-            student.last_name,
-            student.patronymic,
-            student.studying_group_id,
+        data = [
+            groups.pk,
+            groups.group_number,
+            left_students
         ]
+        data2 = groups.student_set.all().values()
 
         # Assign the data for each cell of the row
-        for col_num, cell_value in enumerate(row, 1):
+        for col_num, cell_value in enumerate(data, 1):
             cell = worksheet2.cell(row=row_num, column=col_num)
             cell.value = cell_value
+            cell.alignment = wrapped_alignment
 
-    #set auto width to cells
-    for column_cells in worksheet.columns:
-        length = max(len(str(cell.value)) for cell in column_cells)
-        worksheet.column_dimensions[column_cells[0].column_letter].width = length
+        for col_num, cell_value in enumerate(data2, 1):
+            cell = worksheet2.cell(row=row_num, column=len(data) + 1)
+            cell.value = cell_value['first_name'] + " " + cell_value['last_name']
+            cell.alignment = wrapped_alignment
+            row_num += 1
+
+        if not data2:
+            row_num += 1
+
+    def column_dimensions_width(column: int, width: int, worksheet=None):
+        column_letter = get_column_letter(column)
+        column_dimensions = worksheet.column_dimensions[column_letter]
+        column_dimensions.width = width
+
+    column_dimensions_width(2, 20, worksheet)
+    column_dimensions_width(3, 20, worksheet)
+    column_dimensions_width(4, 40, worksheet)
+    column_dimensions_width(2, 20, worksheet2)
+    column_dimensions_width(3, 20, worksheet2)
+    column_dimensions_width(4, 20, worksheet2)
 
     workbook.save(response)
 
